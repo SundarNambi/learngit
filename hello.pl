e38a9376fa183e856ad84459616ecc63b4eaa5f6
sundar <n_sundar@live.com>
2025-07-11 13:32:30 +0000

    initial

---
+++ hello.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+#
+## This is a simple Perl script to print "Hello, World!"
+#
+#use strict;      # Enables strict mode
+#
+#use warnings;    # Enables warnings
+#
+print "Hello, World!\n";  # Print statement with newline
